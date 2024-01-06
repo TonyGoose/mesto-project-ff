7fbf605d9ef5d82d99e16e9e381206e0ec6ad1bc
Anton Golovanov <preparefight@yandex.ru>
2024-01-06 19:05:39 +0300

    issue_fix

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,38 +1,21 @@
-export { CloseModalKey, showPopupHandle, closePopupHandle}
+export { CloseModalKey, showPopupHandle, closePopupHandle };
 
-const popupList = Array.from(document.querySelectorAll('.popup'));
+function showPopupHandle(popup) {
+  popup.classList.add("popup_is-opened");
 
-popupList.forEach((popup) => {
-  popup.addEventListener('mouseup', (event) => {
-    const targetClassList = event.target.classList; 
-    if (targetClassList.contains('popup') || targetClassList.contains('popup__close')) {
-      closePopupHandle(popup); 
-    }
-  })
-}) 
+  document.addEventListener("keydown", CloseModalKey);
+}
 
-// Закрытие по клику на escape
-
+function closePopupHandle() {
+  const popup = document.querySelector(".popup_is-opened");
+  popup.classList.remove("popup_is-opened");
+  document.removeEventListener("keydown", CloseModalKey);
+}
 
 function CloseModalKey(evt) {
   if (evt.code === "Escape") {
-    const popup = document.querySelector('.popup_is-opened')
-    popup.classList.remove("popup_is-opened");
+    closePopupHandle();
   }
 }
 
-// Открытие попапов
 
-function showPopupHandle(popup) {
-  
-  popup.classList.add("popup_is-opened");
-
-  document.addEventListener('keydown', CloseModalKey);
-}
-
-// Закрытие  со снятием обработчиков
-
-function closePopupHandle(popup) {
-  popup.classList.remove("popup_is-opened");
-  document.removeEventListener('keydown', CloseModalKey);
-}
