2c9d4dcd01eee42089c31e500dc3ca23d8ff860c
Anton Golovanov <preparefight@yandex.ru>
2024-03-17 19:35:10 +0300

    правка api

--- src/components/api.js
+++ src/components/api.js
@@ -1,80 +1,80 @@
 const config = {
-  Url: "https://nomoreparties.co/v1/wff-cohort-8",
+  baseUrl: "https://nomoreparties.co/v1/wff-cohort-8",
   headers: {
     authorization: "fb3eb883-7ac4-4273-84f2-c6471c46b3b2",
     "Content-Type": "application/json",
   },
 };
 
+function request(url, options) {
+  return fetch(url, options).then(handleResponse);
+}
+
 const updateUserAvatar = (avatar) => {
-  return fetch(`${config.Url}/users/me/avatar`, {
+  return request(`${config.baseUrl}/users/me/avatar`, {
     method: "PATCH",
     headers: config.headers,
     body: JSON.stringify({
       avatar: avatar,
     }),
-  }).then(handleResponse);
+  });
 };
 
 const unLikeCard = (cardID) => {
-  return fetch(`${config.Url}/cards/likes/${cardID}`, {
+  return request(`${config.baseUrl}/cards/likes/${cardID}`, {
     method: "DELETE",
     headers: config.headers,
-  }).then(handleResponse);
+  });
 };
 
 const likeCard = (cardID) => {
-  return fetch(`${config.Url}/cards/likes/${cardID}`, {
+  return request(`${config.baseUrl}/cards/likes/${cardID}`, {
     method: "PUT",
     headers: config.headers,
-  }).then(handleResponse);
+  });
 };
 
 const getInitialCards = () => {
-  return fetch(`${config.Url}/cards`, {
+  return request(`${config.baseUrl}/cards`, {
     method: "GET",
     headers: config.headers,
-  }).then(handleResponse);
+  });
 };
 
 const getUserData = () => {
-  return fetch(`${config.Url}/users/me`, {
+  return request(`${config.baseUrl}/users/me`, {
     method: "GET",
     headers: config.headers,
-  }).then(handleResponse);
+  });
 };
 
-// const updateUserProfile = (name, job) => {
-//   return fetch(`${config.Url}/users/me`, {
-//     method: "PATCH",
-//     headers: config.headers,
-//     body: JSON.stringify({
-//       name: name,
-//       about: job,
-//     }),
-//   }).then(handleResponse);
-// };
-
-function updateUserProfile (name, about) {
-  return getUserData("users/me", "PATCH", { name: name, about: about });
-}
+const updateUserProfile = (name, job) => {
+  return request(`${config.baseUrl}/users/me`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify({
+      name: name,
+      about: job,
+    }),
+  });
+};
 
 const addNewCard = (name, link) => {
-  return fetch(`${config.Url}/cards`, {
+  return request(`${config.baseUrl}/cards`, {
     method: "POST",
     headers: config.headers,
     body: JSON.stringify({
       name: name,
       link: link,
     }),
-  }).then(handleResponse);
+  });
 };
 
 const deleteCard = (cardID) => {
-  return fetch(`${config.Url}/cards/${cardID}`, {
+  return request(`${config.baseUrl}/cards/${cardID}`, {
     method: "DELETE",
     headers: config.headers,
-  }).then(handleResponse);
+  });
 };
 
 const handleResponse = (res) => {
@@ -94,3 +94,4 @@ export {
   getUserData,
   getInitialCards,
 };
+
